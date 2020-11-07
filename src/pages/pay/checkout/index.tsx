32ae5642178aa0f153c40f1ab0f1961c88fbdb45
Paulo Rodrigues <paulo.rodrigues@insole.com.br>
2020-11-07 19:18:15 -0300

    Feat:Adicionando estrutura do projeto

---
+++ src/pages/pay/checkout/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Checkout: React.FC = () => {
+  return (
+    <div>
+      <p>Checkout</p>
+    </div>
+  )
+}
+
+export default Checkout
---
+++ src/pages/pay/pending/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pending: React.FC = () => {
+  return (
+    <div>
+      <p>Pending</p>
+    </div>
+  )
+}
+
+export default Pending
